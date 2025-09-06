0dda0c632aadd844c65b71532dfb021c67742739
ArpitSharma9911 <arpitrohitsharma9911@gmail.com>
2025-09-06 22:50:24 +0530

    Create Task2.py

---
+++ Task2.py
@@ -0,0 +1,8 @@
+#Task2 : Demonstrate List Slicing 
+l1=list(range(11))
+print(l1)
+first5=l1[:5]
+print(first5)
+rev_first5=list(reversed(first5))
+print(rev_first5)
+print("The extracted list is :{} and reversed is :{}".format(first5,rev_first5))
